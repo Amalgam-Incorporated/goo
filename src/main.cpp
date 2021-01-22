dbd563f57e712a454cdb549e174c818300631044
Carl Karsten <carl@personnelware.com>
2021-01-22 00:25:05 -0600

    full screen, centered

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,7 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1000,1000,OF_FULLSCREEN);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+	ofSetupOpenGL(1000,1000,OF_GAME_MODE);			// <-------- setup the GL context
 	ofRunApp(new ofApp());
 
 }
